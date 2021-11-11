9928a68478968209e2b9f56ad53c5f52a66ed0d8
AlysonPereira <alysonrichard2001@gmail.com>
2021-11-11 08:53:29 -0300

    Projeto restaurant-search finalizado

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Home from './pages/Home/';
+import theme from './theme'
+import { Provider } from 'react-redux'
+import { ThemeProvider } from 'styled-components'
+import { Reset } from 'styled-reset'
+import store from './redux/store'
+
+
+function App() {
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+      <Reset/>
+      <Home/>
+      </ThemeProvider>
+    </Provider>
+    
+  )
+}
+
+export default App;
---
+++ src/components/ImageCard/index.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import Skeleton from '../Skeleton'
+
+const Card = styled.div `
+  display : flex ;
+  justify-content : center ;
+  padding : 5px ;
+  widht : 90px ;
+  height : 90px ;
+  border-radius : 6px ;
+  background-image : url(${(props) => props.photo});
+  background-size : cover ;
+`
+const Title = styled.span `
+  font-family : ${(props) => props.theme.fonts.text} ;
+  color : #ffffff ;
+  font-size : 16px ;
+`
+
+const ImageCard = ({ photo, title }) => {
+  const [imageLoaded,setImageLoaded] = useState(false)
+  useEffect(() => {
+    const imageLoaded = new Image()
+    imageLoaded.src = photo
+    imageLoaded.onload = () => setImageLoaded(true)
+  },[photo])
+  return (
+    <>
+      {imageLoaded ? (
+        <Card photo = {photo}>
+          <Title>{title}</Title>
+        </Card>
+      ) : (
+        <Skeleton width='90px' height='90px' />
+      )}
+    </>
+  )
+  
+}
+
+export default ImageCard
